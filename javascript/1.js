9bff004d1791a5efb84a78e29529c57a1fbfd74b
0_Main <ifnottrue.continue@gmail.com>
2018-11-24 03:03:21 -0800

    First Commit

---
+++ javascript/1.js
@@ -0,0 +1,64 @@
+//$(document).ready(function() {
+//    $(window).scroll( function(){
+//        $('.col-lg-3').each( function(i){
+//            
+//            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//            var bottom_of_window = $(window).scrollTop() + $(window).height();
+//            
+//            /* If the object is completely visible in the window, fade it it */
+//            if( bottom_of_window > bottom_of_object ){
+//                $(this).animate({'opacity':'1'},1750);                  
+//            }            
+//        }); 
+//    });
+//});
+
+
+//$(document).ready(function(){
+////    if variable in viewport:
+//        $(".row").on(scroll, fadeIn('8000');
+//    
+//});
+//    
+                       
+                
+//$('document').ready(function(){
+//   $('.col-lg-3').fadeOut(500);
+//   $('.col-lg-3').fadeIn(500);
+//});
+    
+//$(document).ready(function() {
+//    
+//    /* Every time the window is scrolled ... */
+//    $(window).scroll( function(){
+//    
+//        /* Check the location of each desired element */
+//        $('.row').each( function(i){
+//            
+//            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+//            var bottom_of_window = $(window).scrollTop() + $(window).height();
+//            
+//            /* If the object is completely visible in the window, fade it it */
+//            if( bottom_of_window > bottom_of_object ){
+//                
+//                $(this).animate({'opacity':'1'},500);
+//                    
+//            }
+//            
+//        }); 
+//    
+//    });
+//    
+//});
+
+
+
+//$('document').ready(function(){
+//   $('.col-lg-3').click(function(){
+//       $('this').fadeOut();
+//   });
+//});
+//   
+
+
+h = $('window');
